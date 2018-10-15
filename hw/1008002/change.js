d0f4f00b0f74c4b3ce165665025a90ea43a49127
yout <930222789@qq.com>
2018-10-15 20:32:37 +0800

    Add files via git

--- hw/1008002/change.js
+++ hw/1008002/change.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-	var images = ['url(img/01-2.jpg)', 'url(img/01-3.jpg)', 'url(img/01-1.jpg)']
+	var images = ['url(./img/01-2.jpg)', 'url(./img/01-3.jpg)', 'url(./img/01-1.jpg)']
 	var index = 0;
 	setInterval(function() {
 		side.style.backgroundImage = images[index++];
