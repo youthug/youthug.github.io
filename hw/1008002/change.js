d490ffd07cfb1bd5d0e0e8b596afcb7bbd76ac1c
yout <930222789@qq.com>
2018-10-15 20:25:58 +0800

    Add files via git

---
+++ hw/1008002/change.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+	var images = ['url(img/01-2.jpg)', 'url(img/01-3.jpg)', 'url(img/01-1.jpg)']
+	var index = 0;
+	setInterval(function() {
+		side.style.backgroundImage = images[index++];
+		if (index == images.length) index = 0;
+	}, 5000);
+};
